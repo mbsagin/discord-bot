061dd22188b1fa27f5b04e646c212705414f3c28
mrsagn <mbsagin@gmail.com>
2020-01-15 22:37:01 +0300

    Token Update

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=C:\Users\beraa\bert-bot"
+set "VIRTUAL_ENV=C:\Users\beraa\discord-bot"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
@@ -13,7 +13,7 @@ if defined _OLD_VIRTUAL_PROMPT (
     )
 )
 if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-    set "PROMPT=(bert-bot) %PROMPT%"
+    set "PROMPT=(discord-bot) %PROMPT%"
 )
 
 REM Don't use () to avoid problems with them in %PATH%
--- Scripts/activate.xsh
+++ Scripts/activate.xsh
@@ -28,7 +28,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"C:\Users\beraa\bert-bot"
+$VIRTUAL_ENV = r"C:\Users\beraa\discord-bot"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
